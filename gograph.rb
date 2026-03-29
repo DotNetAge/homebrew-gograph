3a25948de03221ef9746e7076fe5bd9e70e8fd85
Ray <csharp2002@hotmail.com>
2026-03-29 17:42:45 +0800

    Update Gograph to version v0.2.0

--- gograph.rb
+++ gograph.rb
@@ -1,26 +1,26 @@
 class Gograph < Formula
   desc "Pure Go embedded graph database"
   homepage "https://github.com/DotNetAge/gograph"
-  version "0.1.0"
+  version "v0.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/DotNetAge/gograph/releases/download/v0.1.0/gograph-v0.1.0-4-g3f04068-darwin-arm64.tar.gz"
-      sha256 "bf0e8b3cea6154b2228b3fbb0020861f1ce74a23ec9a43d3277335ee0bb4be0f"
+      url "https://github.com/DotNetAge/gograph/releases/download/v0.2.0/gograph-v0.2.0-darwin-arm64.tar.gz"
+      sha256 "53e676ecc084de771e5deec75e96962bd11654b5085302ba00ee5585d2e50adb"
     else
-      url "https://github.com/DotNetAge/gograph/releases/download/v0.1.0/gograph-v0.1.0-4-g3f04068-darwin-amd64.tar.gz"
-      sha256 "15baaa4c86a27de182cf79194b60a13f7788f7ac961f8ef2b8ebb4eee1a1bb92"
+      url "https://github.com/DotNetAge/gograph/releases/download/v0.2.0/gograph-v0.2.0-darwin-amd64.tar.gz"
+      sha256 "3e4771b1fc148e68ec1d3ec3eda1c5a5bb1b086de9d61292086ff7be2de06b48"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/DotNetAge/gograph/releases/download/v0.1.0/gograph-v0.1.0-4-g3f04068-linux-arm64.tar.gz"
-      sha256 "bc3ddd14153f5321a8b1ee05c03f65b009db698424f4e386cf93e881619cfb7f"
+      url "https://github.com/DotNetAge/gograph/releases/download/v0.2.0/gograph-v0.2.0-linux-arm64.tar.gz"
+      sha256 "bacd8831cd55093e257a79f1fc82dc5b906e8049d50d20ea86de0635e7a6de76"
     else
-      url "https://github.com/DotNetAge/gograph/releases/download/v0.1.0/gograph-v0.1.0-4-g3f04068-linux-amd64.tar.gz"
-      sha256 "3478c94eb828e85fd931af6d7e5cbb038c085c30c447f9664c6b2845d8dec3b7"
+      url "https://github.com/DotNetAge/gograph/releases/download/v0.2.0/gograph-v0.2.0-linux-amd64.tar.gz"
+      sha256 "d93665425264cfd25319d5cb8f1cd67c76fae55011e1c07060d18f7cab475d6d"
     end
   end
 

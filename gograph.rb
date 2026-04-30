0b9868e7cd556c7f12fb236bc8b214ef671e1610
Ray <csharp2002@hotmail.com>
2026-04-30 23:49:16 +0800

    Update gograph to version 0.2.3

--- gograph.rb
+++ gograph.rb
@@ -1,26 +1,26 @@
 class Gograph < Formula
   desc "Pure Go embedded graph database"
   homepage "https://github.com/DotNetAge/gograph"
-  version "v0.2.2"
+  version "v0.2.3"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/DotNetAge/gograph/releases/download/v0.2.2/gograph-v0.2.2-darwin-arm64.tar.gz"
-      sha256 "3fc84325ae1197394ef8d70d9d38d6c018d8d01fd2a40d2fc0b4d3da236a5fb5"
+      url "https://github.com/DotNetAge/gograph/releases/download/v0.2.3/gograph-v0.2.3-darwin-arm64.tar.gz"
+      sha256 "921147676802656b1cb3770a0a6261edcc890de1ba274b1e5222abfa9819a16c"
     else
-      url "https://github.com/DotNetAge/gograph/releases/download/v0.2.2/gograph-v0.2.2-darwin-amd64.tar.gz"
-      sha256 "34e42cd431fa57a4658780f53fef49cbf5cceff894973d72f8673c53f319d094"
+      url "https://github.com/DotNetAge/gograph/releases/download/v0.2.3/gograph-v0.2.3-darwin-amd64.tar.gz"
+      sha256 "d64b8144e111183704048928ca1415d0df193235f39fdd58a7681b7f71630352"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/DotNetAge/gograph/releases/download/v0.2.2/gograph-v0.2.2-linux-arm64.tar.gz"
-      sha256 "ff14d7d80513be1338fbaaedbcd19efddbdfe80b3929549b072cfd89b8e18707"
+      url "https://github.com/DotNetAge/gograph/releases/download/v0.2.3/gograph-v0.2.3-linux-arm64.tar.gz"
+      sha256 "6aedfba9bd57a298c7dafd69deed27a203381d320346f8d6fb067f63e5471471"
     else
-      url "https://github.com/DotNetAge/gograph/releases/download/v0.2.2/gograph-v0.2.2-linux-amd64.tar.gz"
-      sha256 "4032f31b68584a5b60723748a3899bca478e5eff285e38b0664747715a865866"
+      url "https://github.com/DotNetAge/gograph/releases/download/v0.2.3/gograph-v0.2.3-linux-amd64.tar.gz"
+      sha256 "3444b8763e39a582fa03c208491dd1aba2a362eb1418d3e1076519c3f972ed5c"
     end
   end
 
